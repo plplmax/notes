a32b953a5025ddc4cb60d81cc91c1fcfeebcf33b
plplmax <50287455+plplmax@users.noreply.github.com>
2022-05-20 09:14:49 +0300

    refactor(notes): optimize imports

--- app/src/main/kotlin/com/github/plplmax/notes/ui/base/BaseFragment.kt
+++ app/src/main/kotlin/com/github/plplmax/notes/ui/base/BaseFragment.kt
@@ -26,16 +26,11 @@ package com.github.plplmax.notes.ui.base
 
 import android.content.Context
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.viewbinding.ViewBinding
-import com.github.plplmax.notes.databinding.FragmentSignInBinding
-import com.github.plplmax.notes.databinding.FragmentSignUpBinding
 import com.github.plplmax.notes.ui.core.FragmentListener
 import com.google.android.material.snackbar.Snackbar
 
